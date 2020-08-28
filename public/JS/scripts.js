b5ed54fec9627e8918e23d6094bc421a30021073
rafaelsgomes <rafael_souza.41@hotmail.com>
2020-08-28 18:43:28 -0300

    Starting Upload Images

--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -7,58 +7,21 @@ for(item of menu){
         item.classList.add('active')
     }
 }
-
 //Show or Hide recipe informations
-for (let i = 0; i < document.querySelectorAll('.details-header').length; i++){
-const recipeIngredients = document.querySelector('.recipe-ingredients')
-const recipePreparation = document.querySelector('.recipe-preparation')
-const recipeInformation = document.querySelector('.recipe-information')
-
-function showOrHide(event){
-        event.classList.toggle('hide')
+const ShowOrHide = {
+    showOrHide(event){
+        event.parentNode.classList.toggle('hide')
         if(event.querySelector('span').textContent == 'Mostrar'){
             event.querySelector('span').textContent = 'Esconder'     
         } else{
             event.querySelector('span').textContent = 'Mostrar'
         }
+    },
+    targetEvent(event){
+        const target = event.target.parentNode
+        ShowOrHide.showOrHide(target)
     }
-recipeIngredients.querySelector('.details-header').addEventListener('click', () => showOrHide(recipeIngredients) )
-recipePreparation.querySelector('.details-header').addEventListener('click', () => showOrHide(recipePreparation) )
-recipeInformation.querySelector('.details-header').addEventListener('click', () => showOrHide(recipeInformation) )
 }
-
-// Add Inputs
-const formCreate = document.querySelector('#form-create')
-function addInput(field, container){
-    const fieldInput = field
-    const fieldContainer = container
-    
-    const newInput = fieldContainer[fieldContainer.length -1].cloneNode(true)
-
-    if(newInput.children[0].value == "") return false
-
-    newInput.children[0].value = ''
-    fieldInput.appendChild(newInput)
-}
-
-if(formCreate){
-document.querySelector('.add-ingredient').addEventListener('click', () => 
-addInput(document.querySelector('#ingredients'), document.querySelectorAll('.ingredient')))
-
-document.querySelector('.add-step').addEventListener('click', () => 
-addInput(document.querySelector('#preparations'), document.querySelectorAll('.step')))
-}
-const formDelete = document.querySelector('#form-delete')
-
-if(formDelete){
-    // delete confirmation
-document.querySelector('#form-delete').addEventListener('submit', (event)=>{
-    const confirmation = confirm('Deseja realmente deletar?')
-    if(!confirmation) event.preventDefault()
-})
-}
-
-
 // Pagination
 const pagination = document.querySelector('.pagination')
 function paginate(pagination){
@@ -169,9 +132,8 @@ function paginate(pagination){
             return {left, right}
         }
     }
-        controls.listeners()
-        buttons.update() 
-
+    controls.listeners()
+    buttons.update() 
     function update(){
         buttons.update() 
     }
@@ -182,5 +144,124 @@ if(pagination){
     const paginationNumbers = document.querySelectorAll('.pages-list div')
     for (item of paginationNumbers){
     item.classList.add('item-control')
+    }
+}
+
+// Add Inputs
+const AddInputs = {
+    addInput(field, container){
+        const fieldInput = field
+        const fieldContainer = container
+        
+        const newInput = fieldContainer[fieldContainer.length -1].cloneNode(true)
+    
+        if(newInput.children[0].value == "") return false
+    
+        newInput.children[0].value = ''
+        fieldInput.appendChild(newInput)
+    },
+    buttonAdd(event){
+        const target = event.target.parentNode.children[1]
+        const ingredient = document.querySelectorAll('.ingredient')
+        const preparation =  document.querySelectorAll('.step')
+        if(target.getAttribute("id") == "ingredients"){
+            AddInputs.addInput(target, ingredient)
+        }else{
+            AddInputs.addInput(target, preparation)
+        }
+    }
+}
+// Delete Confirmation
+const formDelete = document.querySelector('#form-delete')
+if(formDelete){  
+    document.querySelector('#form-delete').addEventListener('submit', (event)=>{
+        const confirmation = confirm('Deseja realmente deletar?')
+        if(!confirmation) event.preventDefault()
+    })
+}
+
+const ImagesUpload = {
+    preview: document.querySelector('#images-preview'),
+    limit: 5,
+    input: '',
+    files: [],
+    filesInput(event){
+        const {files: fileList} = event.target
+        const {preview} = ImagesUpload
+
+        ImagesUpload.input = event.target
+
+        if(ImagesUpload.uploadLimit(event)) return
+
+        Array.from(fileList).forEach(file =>{
+            ImagesUpload.files.push(file)
+
+            const reader = new FileReader()
+            reader.onload = ()=>{   
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const container = document.createElement('div')
+                container.classList.add('image')
+
+                container.onclick = ImagesUpload.removeImage
+                container.appendChild(image)
+                container.appendChild(ImagesUpload.removeButton())
+
+               preview.appendChild(container)
+            }
+            reader.readAsDataURL(file)
+        })
+        ImagesUpload.input.files = ImagesUpload.allFiles()
+    },
+    allFiles(){
+        const dataTransfer = new ClipboardEvent('').clipboardData || new DataTransfer()
+
+        ImagesUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
+    },
+    uploadLimit(event){
+        const {limit, input, preview} = ImagesUpload
+        const {files: fileList} = input
+        
+        if(fileList.length > limit){
+            alert(`Envie no máximo ${limit} imagens`)
+            event.preventDefault()
+            return true
+        }
+
+        const imagesArray = []
+        preview.childNodes.forEach(item => {
+            if(item.classList && item.classList.value == "image"){
+                imagesArray.push(item)
+            }
+        })
+
+        const totalImages = fileList.length + imagesArray.length
+
+        if(totalImages > limit){
+            alert(`Envie no máximo ${limit} imagens`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    removeButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "delete_outline"
+        return button
+    },
+    removeImage(event){
+        const imageDiv = event.target.parentNode
+        const imagesArray = Array.from(ImagesUpload.preview.children)
+        const index = imagesArray.indexOf(imageDiv)
+
+        ImagesUpload.files.splice(index, 1)
+        ImagesUpload.input.files = ImagesUpload.allFiles()
+
+        imageDiv.remove()
     }
 }
